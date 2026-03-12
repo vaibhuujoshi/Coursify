2cebbdcc17fc9fee6f1636e2601f26e7fda7a126
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-12 23:23:15 +0530

    Fix: Schema of firstName

--- models/admin.js
+++ models/admin.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 const AdminSchema = new Schema({
     email: { type: String, Unique: true, required: true },
     password: { type: String, required: true },
-    firstName: String,
+    firstName: { type: String, required: true },
     lastName: String
 })
 
