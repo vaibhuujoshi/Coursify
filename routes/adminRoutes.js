325de343cde7c35042f79285e3cbf0803bb5f81e
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-12 23:46:39 +0530

    Add: authorization middleware

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
+import authorization from "../middleware/authorizationMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.get("/me", auth, getAdmin);
+router.get("/me", auth, authorization, getAdmin);
 // add authorization middleware for admin
 
 export default router;
