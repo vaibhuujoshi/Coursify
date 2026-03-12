eacc6bd1b85cd6035c4db066dc091b93aa23c546
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-12 22:30:11 +0530

    Add: admin signin and getAdmin

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -33,11 +33,11 @@ async function signup(req, res) {
 
         const { email, password, firstName, lastName } = req.body;
 
-        const user = await AdminModel.findOne({
+        const admin = await AdminModel.findOne({
             email
         })
 
-        if (user) {
+        if (admin) {
             return res.status(409).json({
                 message: "Admin already exits"
             })
@@ -63,3 +63,63 @@ async function signup(req, res) {
     }
 }
 
+async function signin(req, res) {
+    try {
+        const requiredBody = z.object({
+            email: z.string().min(3).max(100).email(),
+            password: z.string()
+                .min(8, { message: "Password should have minimum length of 8" })
+                .max(15, "Password is too long")
+                .regex(/^(?=.*[A-Z]).{8,}$/, {
+                    message:
+                        "Should Contain at least one uppercase letter and have a minimum length of 8 characters.",
+                })
+        });
+
+        const parsedWithSuccess = requiredBody.safeParse(req.body);
+
+        if (!parsedWithSuccess.success) {
+            return res.status(403).json({
+                message: "Invalid Format"
+            })
+        }
+
+        const { email, password } = req.body;
+
+        const admin = await AdminModel.findOne({
+            email
+        })
+
+        if (!admin) {
+            return res.status(409).json({
+                message: "Admin does not exist"
+            })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, admin.password);
+
+        if (!passwordMatch) {
+            return res.status(403).json({
+                message: "Wrong password"
+            })
+        }
+
+        const token = generateToken(admin._id, "admin");
+
+        res.status(200).json({
+            message: "Admin is signed in successfully",
+            token
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "There is some error from server side"
+        })
+    }
+}
+
+function getAdmin(req, res) {
+    res.status(200).json(req.userId);
+}
+
+export {signup, signin, getAdmin};
