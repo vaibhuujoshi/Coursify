5ba75ad40b0a815fb961aa1c7ac5adf6b6cff777
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-12 23:19:54 +0530

    FIx: import files

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { signin, signup, getAdmin } from "../controllers/adminController";
-import auth from "../middleware/authMiddleware";
+import { signin, signup, getAdmin } from "../controllers/adminController.js";
+import auth from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/me", auth, getAdmin);
+// add authorization middleware for admin
 
 export default router;
