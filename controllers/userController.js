4a32a47777f49b35a1c61bee4a85577d5a591662
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-12 13:16:06 +0530

    Fix: await in password match

--- controllers/userController.js
+++ controllers/userController.js
@@ -101,7 +101,7 @@ async function signin(req, res) {
             })
         }
 
-        const passwordMatch = bcrypt.compare(password, user.password);
+        const passwordMatch = await bcrypt.compare(password, user.password);
 
         if (!passwordMatch) {
             return res.status(403).josn({
