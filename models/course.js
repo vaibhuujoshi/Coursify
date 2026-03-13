bbf18c0debd637b706de4905ff006a4aa8a9619f
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-13 10:25:26 +0530

    Add: Course Schema

---
+++ models/course.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const CourseSchema = new Schema({
+    title: {type: String, required: true},
+    description: {type: String, required: true},
+    price: {type: Number, required: true},
+    imageUrl: {type: String, required: true},
+    creatorId: {type: Schema.Types.ObjectId, ref: 'admins'}
+})
+
+const CourseModel = mongoose.model('courses', CourseSchema);
+
+export default CourseModel;
