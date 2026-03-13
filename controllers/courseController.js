244bd8c749a8acc9532568eceded8d111f0a04bd
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-13 11:19:44 +0530

    Add: course creating logic

---
+++ controllers/courseController.js
@@ -0,0 +1,42 @@
+import CourseModel from "../models/course";
+import { z } from "zod";
+
+async function course(req, res) {
+    try {
+        const requiredBody = z.object({
+            title: z.string().min(1).max(100),
+            description: z.string().min(0).max(100),
+            price: z.number().min(1),
+            imageUrl: z.string().min(0).max(100)
+        });
+
+        const parsedWithSuccess = requiredBody.safeParse(req.body);
+
+        if (!parsedWithSuccess.success) {
+            return res.status(400).json({
+                message: "Invalid Format"
+            })
+        }
+
+        const { title, description, price, imageUrl } = req.body;
+
+        const creatorId = req.user.id;
+
+        await CourseModel.create({
+            title,
+            description,
+            price,
+            imageUrl,
+            creatorId
+        })
+
+        res.status(200).json({
+            message: "Course created successfully"
+        })
+
+    } catch (err) {
+        return res.status(500).json({
+            message: "There is some error from server side"
+        })
+    }
+}
