d87ebaf26e943d52db424f21f70da3cbe6c9b461
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-14 15:13:40 +0530

    Add: delete course route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
 import authorization from "../middleware/authorizationMiddleware.js";
-import {course, updateCourse} from "../controllers/adminCourseController.js"
+import {course, updateCourse, deleteCourse} from "../controllers/adminCourseController.js"
 
 const router = express.Router();
 
@@ -11,5 +11,6 @@ router.post("/signin", signin);
 router.get("/me", auth, authorization, getAdmin);
 router.post("/course", auth, authorization, course);
 router.put("/course/:courseId", auth, authorization, updateCourse);
+router.delete("/course/:courseId", auth, authorization, deleteCourse);
 
 export default router;
