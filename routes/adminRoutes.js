50862aa0faa3249ee3de3965727e6be7099ec2bb
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-15 22:32:45 +0530

    Add: getCourse route

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
 import authorization from "../middleware/authorizationMiddleware.js";
-import {course, updateCourse, deleteCourse} from "../controllers/adminCourseController.js"
+import {course, updateCourse, deleteCourse, getCourses} from "../controllers/adminCourseController.js"
 
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.post("/signup", signup);
 router.post("/signin", signin);
 router.get("/me", auth, authorization, getAdmin);
 router.post("/course", auth, authorization, course);
+router.get("/courses", auth, authorization, getCourses);
 router.put("/course/:courseId", auth, authorization, updateCourse);
 router.delete("/course/:courseId", auth, authorization, deleteCourse);
 
