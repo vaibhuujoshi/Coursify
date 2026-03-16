9076d4678aefd601fe4d1ce9e7e281bd5f072393
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-16 10:40:19 +0530

    Add: courses routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { signin, signup, getUser } from "../controllers/userController.js";
 import auth from "../middleware/authMiddleware.js";
+import { getCourses, getACourse } from "../controllers/userCourseController.js";
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/signin', signin);
 router.get('/me', auth, getUser);
+
+router.get('/courses', auth, getCourses);
+router.get('/course/:courseId', auth, getACourse);
 
 export default router;
