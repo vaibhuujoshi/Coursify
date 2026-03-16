50abd903302d325cf71190349db8b43c16fb3021
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-17 00:13:38 +0530

    Add: purchasing course routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { signin, signup, getUser } from "../controllers/userController.js";
 import auth from "../middleware/authMiddleware.js";
 import { getCourses, getACourse } from "../controllers/userCourseController.js";
+import { purchaseCourse, getPurchasedCourses } from "../controllers/purchaseCourseController.js";
 
 const router = express.Router();
 
@@ -11,5 +12,8 @@ router.get('/me', auth, getUser);
 
 router.get('/courses', auth, getCourses);
 router.get('/course/:courseId', auth, getACourse);
+
+router.post('/course/:courseId', auth, purchaseCourse);
+router.get('/purchasedCourses', auth, getPurchasedCourses);
 
 export default router;
