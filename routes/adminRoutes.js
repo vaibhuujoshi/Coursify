8c44a72571094f68549fa759c93d3db7db7c5bae
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-18 19:25:08 +0530

    Add: protected routes with rateLimiting

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,16 +2,17 @@ import express from "express";
 import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
 import authorization from "../middleware/authorizationMiddleware.js";
-import {course, updateCourse, deleteCourse, getCourses} from "../controllers/adminCourseController.js"
+import { course, updateCourse, deleteCourse, getCourses } from "../controllers/adminCourseController.js"
+import { signupLimiter, loginLimiter, userLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/signin", signin);
-router.get("/me", auth, authorization, getAdmin);
-router.post("/course", auth, authorization, course);
-router.get("/courses", auth, authorization, getCourses);
-router.put("/course/:courseId", auth, authorization, updateCourse);
-router.delete("/course/:courseId", auth, authorization, deleteCourse);
+router.post("/signup", signupLimiter, signup);
+router.post("/signin", loginLimiter, signin);
+router.get("/me", auth, authorization, userLimiter, getAdmin);
+router.post("/course", auth, authorization, userLimiter, course);
+router.get("/courses", auth, authorization, userLimiter, getCourses);
+router.put("/course/:courseId", auth, authorization, userLimiter, updateCourse);
+router.delete("/course/:courseId", auth, authorization, userLimiter, deleteCourse);
 
 export default router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,17 +3,18 @@ import { signin, signup, getUser } from "../controllers/userController.js";
 import auth from "../middleware/authMiddleware.js";
 import { getCourses, getACourse } from "../controllers/userCourseController.js";
 import { purchaseCourse, getPurchasedCourses } from "../controllers/purchaseCourseController.js";
+import { signupLimiter, loginLimiter, userLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
-router.post('/signup', signup);
-router.post('/signin', signin);
-router.get('/me', auth, getUser);
+router.post('/signup', signupLimiter, signup);
+router.post('/signin', loginLimiter, signin);
+router.get('/me', auth, userLimiter, getUser);
 
-router.get('/courses', auth, getCourses);
-router.get('/course/:courseId', auth, getACourse);
+router.get('/courses', auth, userLimiter, getCourses);
+router.get('/course/:courseId', auth, userLimiter, getACourse);
 
-router.post('/course/:courseId', auth, purchaseCourse);
-router.get('/purchasedCourses', auth, getPurchasedCourses);
+router.post('/course/:courseId', auth, userLimiter, purchaseCourse);
+router.get('/purchasedCourses', auth, userLimiter, getPurchasedCourses);
 
 export default router;
