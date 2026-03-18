a8e45f01af3aea3ba38a3a94f8e6367092fe1da1
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-18 19:32:03 +0530

    Fix: userLimiter to adminLimiter

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,16 +3,16 @@ import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
 import authorization from "../middleware/authorizationMiddleware.js";
 import { course, updateCourse, deleteCourse, getCourses } from "../controllers/adminCourseController.js"
-import { signupLimiter, loginLimiter, userLimiter } from "../middleware/rateLimiter.js";
+import { signupLimiter, loginLimiter, adminLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
 router.post("/signup", signupLimiter, signup);
 router.post("/signin", loginLimiter, signin);
-router.get("/me", auth, authorization, userLimiter, getAdmin);
-router.post("/course", auth, authorization, userLimiter, course);
-router.get("/courses", auth, authorization, userLimiter, getCourses);
-router.put("/course/:courseId", auth, authorization, userLimiter, updateCourse);
-router.delete("/course/:courseId", auth, authorization, userLimiter, deleteCourse);
+router.get("/me", auth, authorization, adminLimiter, getAdmin);
+router.post("/course", auth, authorization, adminLimiter, course);
+router.get("/courses", auth, authorization, adminLimiter, getCourses);
+router.put("/course/:courseId", auth, authorization, adminLimiter, updateCourse);
+router.delete("/course/:courseId", auth, authorization, adminLimiter, deleteCourse);
 
 export default router;
