eade320e90b82f1b016c2f095f3a27fef7b87723
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-18 19:27:07 +0530

    Add: admin rate limiter

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -36,6 +36,26 @@ const userLimiter = rateLimit({
     }
 })
 
+const adminLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 500,
+
+    keyGenerator: (req) => {
+        return req.user?.id || req.ip;
+    },
+
+    standardHeaders: true,
+    legacyHeaders: false,
+
+    handler: (req, res) => {
+        return res.status(429).json({
+            success: false,
+            message: "Admin rate limit exceeded. Try again later.",
+            retryAfter: Math.ceil(options.windowMs / 1000) + " seconds"
+        })
+    }
+})
+
 const loginLimiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 mins
     max: 5,
@@ -63,4 +83,4 @@ const signupLimiter = rateLimit({
     }
 });
 
-export { apiLimiter, userLimiter, loginLimiter, signupLimiter };
+export { apiLimiter, userLimiter, loginLimiter, signupLimiter, adminLimiter };
