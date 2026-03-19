6318c3b9e7fce3ad4eb4ec3f47e5f0fe1d7cd01c
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-19 19:49:47 +0530

    Fix: Controller name

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { signin, signup, getAdmin } from "../controllers/adminController.js";
 import auth from "../middleware/authMiddleware.js";
 import authorization from "../middleware/authorizationMiddleware.js";
-import { course, updateCourse, deleteCourse, getCourses } from "../controllers/adminCourseController.js"
+import { createCourseHandler, updateCourseHandler, deleteCourseHandler, getCourses } from "../controllers/adminCourseController.js"
 import { signupLimiter, loginLimiter, adminLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
@@ -10,9 +10,9 @@ const router = express.Router();
 router.post("/signup", signupLimiter, signup);
 router.post("/signin", loginLimiter, signin);
 router.get("/me", auth, authorization, adminLimiter, getAdmin);
-router.post("/course", auth, authorization, adminLimiter, course);
+router.post("/course", auth, authorization, adminLimiter, createCourseHandler);
 router.get("/courses", auth, authorization, adminLimiter, getCourses);
-router.put("/course/:courseId", auth, authorization, adminLimiter, updateCourse);
-router.delete("/course/:courseId", auth, authorization, adminLimiter, deleteCourse);
+router.put("/course/:courseId", auth, authorization, adminLimiter, updateCourseHandler);
+router.delete("/course/:courseId", auth, authorization, adminLimiter, deleteCourseHandler);
 
 export default router;
