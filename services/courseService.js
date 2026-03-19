8c742113f607c94d64a3b892553d39977cd3397e
Vaibhav Joshi <joshivaibhavjoc@gmail.com>
2026-03-19 19:48:41 +0530

    Add: update and delete course service

--- services/courseService.js
+++ services/courseService.js
@@ -1,19 +1,31 @@
 import CourseModel from "../models/course.js";
 
 async function createCourse(data, adminId) {
-
-    const now = new Date();
-    const createdAt = now.toLocaleString('en-IN', {
-        hour12: true
-    });
-
     const course = await CourseModel.create({
         ...data,
-        createdAt,
         creatorId: adminId
     });
 
     return course;
 }
 
-export { createCourse };
+async function updateCourse(data, courseId, adminId) {
+    const courseUpdated = await CourseModel.findOneAndUpdate(
+        { _id: courseId, creatorId: adminId },
+        data,
+        { new: true }
+    )
+
+    return courseUpdated;
+}
+
+async function deleteCourse(courseId, adminId) {
+    const courseDeleted = await CourseModel.findOneAndDelete({
+        _id: courseId,
+        creatorId: adminId
+    })
+
+    return courseDeleted;
+}
+
+export { createCourse, updateCourse, deleteCourse };
